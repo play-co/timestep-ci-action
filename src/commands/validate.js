3f4e717a48fc29e3fcbb126248db9a00762d7ace
Ivan Reshetnikov <keenn2007@gmail.com>
2019-03-06 21:04:26 +0100

    minor cleanup

--- src/commands/validate.js
+++ src/commands/validate.js
@@ -5,7 +5,7 @@ module.exports = async (yargs) => {
   const tools = new Toolkit({
     event: ['pull_request']
   });
-  
+
   tools.log('payload action', tools.context.payload.action);
 
   const result = await execa('npx', [
