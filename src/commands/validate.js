79db8ed9b0ce67c810715250cdb02f489571fd54
Ivan Reshetnikov <keenn2007@gmail.com>
2019-03-21 18:48:24 +0100

    merge updates

--- src/commands/validate.js
+++ src/commands/validate.js
@@ -1,20 +1,44 @@
 const execa = require('execa');
 const { Toolkit } = require('actions-toolkit');
 
+const allowedActions = [
+  'opened', 'edited', 'reopened', 'synchronize'
+];
+
 module.exports = async (yargs) => {
   const tools = new Toolkit({
     event: ['pull_request']
   });
 
-  tools.log('payload action', tools.context.payload.action);
+  const action = tools.context.payload.action;
 
-  const result = await execa('npx', [
-    'tsci', 'changelog', 'validate',
+  tools.log('payload action', action);
+
+  if (!allowedActions.includes(action)) {
+    tools.log('skipping unsupported action: ' + action);
+
+    process.exit(0);
+  }
+
+  const subprocess = execa('npx', [
+    'tsci', 'changelog', 'status',
     '--cwd=' + tools.workspace,
     '--auth-token=' + tools.token,
-    '--pull-request=' + tools.context.payload.pull_request.number,
-    '--output=json'
+    '--pull-request=' + tools.context.payload.pull_request.number
   ]);
 
-  tools.log('tsci validate result', JSON.parse(result.stdout));
+  subprocess.stdout.pipe(process.stdout);
+
+  try {
+    await subprocess;
+
+    tools.log('pull request validation successful!');
+
+    process.exit(0);
+  } catch (error) {
+    tools.log('pull request validation error!');
+    tools.log('tsci exited with code ' + error.exitCode + ': ' + error.message);
+
+    process.exit(1);
+  }
 };
