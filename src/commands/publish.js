da0d66763bff5f1554ab519746fe6cb149e00f89
Ivan Reshetnikov <keenn2007@gmail.com>
2019-04-05 14:27:44 +0200

    fix git credentials

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -25,10 +25,21 @@ module.exports = async (tools) => {
   try {
     const npmRcPath = path.join(tools.workspace, '.npmrc');
 
+    // Make sure we're on the required branch
     await runCommand('git', ['checkout', publishBranch]);
     await runCommand('git', ['pull']);
+
+    // Set up npm to use our token
     await fs.writeFile(npmRcPath, npmRcTemplate);
+
+    // Install dependencies
     await runCommand('npm', ['ci']);
+
+    // Set git credentials
+    await runCommand('git', ['config', 'user.email', '"timestep-ci@gameclosure.com"']);
+    await runCommand('git', ['config', 'user.name', '"Timestep CI"']);
+
+    // Publish new version
     await runCommand('npm', ['run', 'release']);
 
     tools.exit.success('new version published successfully');
