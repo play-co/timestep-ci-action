ab9f7398df90378af79d55cd60d702ecff8818e0
Ivan Reshetnikov <keenn2007@gmail.com>
2019-04-05 13:42:12 +0200

    publish command

---
+++ src/commands/publish.js
@@ -0,0 +1,28 @@
+const publishBranch = process.env.PUBLISH_BRANCH || 'master';
+
+module.exports = async (tools) => {
+  const headBranch = tools.context.payload.pull_request.head.ref;
+  const baseBranch = tools.context.payload.pull_request.base.ref;
+
+  tools.log('head branch', headBranch);
+  tools.log('base branch', baseBranch);
+
+  if (baseBranch !== publishBranch) {
+    tools.exit.neutral('no action required for branch ' + baseBranch);
+  }
+
+  await tools.runInWorkspace('git', ['checkout', publishBranch]);
+  await tools.runInWorkspace('git', ['pull']);
+
+  try {
+    const subprocess = tools.runInWorkspace('npm', ['run', 'release']);
+    subprocess.stdout.pipe(process.stdout);
+
+    await subprocess;
+
+    tools.exit.success('new version published successfully');
+  } catch (error) {
+    tools.log.fatal(error);
+    tools.exit.failure('publish failed');
+  }
+};
