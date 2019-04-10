99b8eaddf038a92423495251d7bc731612c874b6
Ivan Reshetnikov <keenn2007@gmail.com>
2019-04-10 15:37:57 +0200

    temporarily disable publishing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ if (['opened', 'edited', 'reopened', 'synchronize'].includes(payload.action)) {
   require('./commands/validate')(tools);
 } else if (payload.action === 'closed' && payload.pull_request.merged) {
   // Dispatch 'publish' action
-  require('./commands/publish')(tools);
+
+  // Temporarily disabled!
+  // require('./commands/publish')(tools);
+
+  tools.exit.neutral('publishing is temporarily disabled');
 } else {
   tools.log('action: ' + payload.action);
   tools.log('pull_request.merged: ' + payload.pull_request.merged);
