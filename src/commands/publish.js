6269df055c5da257f54892cb6ef0e08ad90a7fe6
Jake Davies <jgdavies24@gmail.com>
2020-09-29 10:56:59 +1000

    playco-packages

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -1,13 +1,15 @@
-const execa = require('execa');
-const path = require('path');
-const fs = require('fs-extra');
+const execa = require("execa");
+const path = require("path");
+const fs = require("fs-extra");
 
-const publishBranch = process.env.PUBLISH_BRANCH || 'master';
-const npmRcTemplate = '//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n' +
-  'unsafe-perm = true';
+const publishBranch = process.env.PUBLISH_BRANCH || "master";
+const npmRcTemplate =
+  "@play-co:registry=https://npm.pkg.github.com/:_authToken=${NPM_TOKEN}\n" +
+  "registry=https://registry.npmjs.org/\n" +
+  "unsafe-perm = true";
 
 module.exports = async (tools) => {
-  function runCommand (cmd, args) {
+  function runCommand(cmd, args) {
     const subprocess = execa(cmd, args, { cwd: tools.workspace });
     subprocess.stdout.pipe(process.stdout);
     return subprocess;
@@ -16,36 +18,40 @@ module.exports = async (tools) => {
   const headBranch = tools.context.payload.pull_request.head.ref;
   const baseBranch = tools.context.payload.pull_request.base.ref;
 
-  tools.log('head branch', headBranch);
-  tools.log('base branch', baseBranch);
+  tools.log("head branch", headBranch);
+  tools.log("base branch", baseBranch);
 
   if (baseBranch !== publishBranch) {
-    tools.exit.neutral('no action required for branch ' + baseBranch);
+    tools.exit.neutral("no action required for branch " + baseBranch);
   }
 
   try {
-    const npmRcPath = path.join(tools.workspace, '.npmrc');
+    const npmRcPath = path.join(tools.workspace, ".npmrc");
 
     // Make sure we're on the required branch
-    await runCommand('git', ['checkout', publishBranch]);
-    await runCommand('git', ['pull']);
+    await runCommand("git", ["checkout", publishBranch]);
+    await runCommand("git", ["pull"]);
 
     // Set up npm to use our token
     await fs.writeFile(npmRcPath, npmRcTemplate);
 
     // Install dependencies
-    await runCommand('npm', ['ci']);
+    await runCommand("npm", ["ci"]);
 
     // Set git credentials
-    await runCommand('git', ['config', 'user.email', '"timestep-ci@gameclosure.com"']);
-    await runCommand('git', ['config', 'user.name', '"Timestep CI"']);
+    await runCommand("git", [
+      "config",
+      "user.email",
+      '"timestep-ci@gameclosure.com"',
+    ]);
+    await runCommand("git", ["config", "user.name", '"Timestep CI"']);
 
     // Publish new version
-    await runCommand('npm', ['run', 'release']);
+    await runCommand("npm", ["run", "release"]);
 
-    tools.exit.success('new version published successfully');
+    tools.exit.success("new version published successfully");
   } catch (error) {
     tools.log.fatal(error);
-    tools.exit.failure('publish failed');
+    tools.exit.failure("publish failed");
   }
 };
